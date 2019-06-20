64c9e23106c4dcc6babbee1c1d197833bb7dec30
h4rdc0r3d <49732392+h4rdc0r3d@users.noreply.github.com>
2019-06-20 23:23:32 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,75 @@
+const Commando = require("discord.js-commando");
+const discord = require("discord.js");
+const bot = new Commando.CommandoClient();
+var TOKEN = "NTkxMzU3ODA3ODcwMDgzMDky.XQvnsQ._kJRGUD6U1S6_ccahl4DXbpWLGo";
+const http = require("http");
+
+bot.login(TOKEN);
+
+bot.registry.registerGroup("simple", "Simple");
+bot.registry.registerGroup("music", "Music");
+bot.registry.registerGroup("team", "Team");
+bot.registry.registerGroup("intermediate", "Intermediate");
+bot.registry.registerGroup("fantasy", "Fantasy");
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+// 모든 곳에서 사용하기 위한 전역변수
+global.currentTeamMembers = [];
+global.server = new Map();
+
+bot.on("message", function(message) {
+  if (message.content === "여어") {
+    //message.reply("히사시부리~");
+    message.channel.sendMessage("히사시부리" + message.author + "... AYAYA");
+  } else if (message.content === "팀알려줘") {
+    var teamInfo = new discord.RichEmbed().setTitle("현재 팀");
+    for (var i = 0; i < currentTeamMembers.length; i++) {
+      teamInfo.addField(
+        "팀 멤버: " + (i + 1).toString(),
+        currentTeamMembers[i].username
+      );
+    }
+
+    message.channel.send(teamInfo);
+    //message.channel.send(currentTeamMembers);
+  }
+
+  if (message.content === "inv") {
+    message.member.send("Hello");
+    let memberRole = message.member.guild.roles.find(
+      role => role.name === "New Player"
+    );
+    message.member.addRole(memberRole).catch(console.error);
+
+    // message.member.guild
+    //   .createRole({
+    //     name: message.member.user.username,
+    //     color: "#DC143C",
+    //     permissions: []
+    //   })
+    //   .then(function(role) {
+    //     message.member.addRole(role);
+    //   });
+  }
+});
+
+bot.on("ready", function() {
+  console.log("Ready");
+});
+//create a server object:
+
+bot.on("guildMemberAdd", function(member) {
+  member.send("^ㅁ^");
+  let memberRole = member.guild.roles.find(
+    role => role.name === "New Player"
+  );
+  member.addRole(memberRole);
+});
+
+http
+  .createServer(function(req, res) {
+    res.write("Hello rd!"); //write a response to the client
+    res.end(); //end the response
+  })
+  .listen(8080); //
